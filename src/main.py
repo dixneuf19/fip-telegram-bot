afa1f2d946b885f701d382a6af656bfa7b89e0d5
Julen Dixneuf <julen@student.ecp.fr>
2020-05-11 09:45:25 +0200

    Remove replies

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 from telegram.ext import Updater, CommandHandler
-from telegram import Audio
+from telegram import ParseMode
 from dotenv import load_dotenv
 
 from src.api import get_live_on_FIP, LiveFIPException
@@ -11,7 +11,8 @@ from src.utils import dict_to_simple_song
 from src.fmt import song_to_markdown
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                     level=logging.INFO)
 
 BOT_TELEGRAM_TOKEN = os.getenv("BOT_TELEGRAM_TOKEN")
 
@@ -23,33 +24,50 @@ def get_live(update, context):
         # get the song from FIP
         song = get_live_on_FIP()
         # song = dict_to_simple_song({"title": "Mango meat", "artist": "Mandrill"})
-        update.message.reply_markdown_v2(song_to_markdown(song), disable_web_page_preview=True)
+        
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=song_to_markdown(song),
+            parse_mode=ParseMode.MARKDOWN_V2,
+            disable_web_page_preview=True,
+            )
 
         # search the song on spotify
         spotify_track = get_song_from_spotify(song)
         if spotify_track is None:
-            update.message.reply_markdown_v2("Not found on spotify")
-        else:
-            update.message.reply_text(
-                f"""
-                Found this on Spotify :\n\n{generate_link_from_uri(spotify_track.uri)}
-                """
+            context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="Not found on spotify",
+            parse_mode=ParseMode.MARKDOWN_V2,
             )
-        
+        else:
+            spot_txt = f"""Found this on Spotify :\n\n{generate_link_from_uri(spotify_track.uri)}"""
+            
+            context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=spot_txt,
+            )
         
         logging.warning(spotify_track)
     except LiveFIPException:
-        update.message.reply_markdown_v2("The FIP API cannot inform us about the live, is it _Club Jazzafip_ ?")
+         context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=song_to_markdown("The FIP API cannot inform us about the live, is it _Club Jazzafip_ ?"),
+            parse_mode=ParseMode.MARKDOWN_V2,
+            )
     except Exception as e:
         logging.error(e)
-        update.message.reply_text("Hum something went wrong...")
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=song_to_markdown("Hum something went wrong..."),
+            )
 
 def display_help(update, context):
     help_message = """
 This bot helps you share your love of FIP !
 
 Commands :
-/live - Display the current song played on FIP, and search for it on Spotify. Doesn't work during shows such Jazzafip
+/live, /whatsonFIP - Display the current song played on FIP, and search for it on Spotify. Doesn't work during shows such Jazzafip
     """
     update.message.reply_text(help_message)
 
@@ -57,6 +75,7 @@ Commands :
 updater = Updater(BOT_TELEGRAM_TOKEN, use_context=True)
 
 updater.dispatcher.add_handler(CommandHandler("live", get_live))
+updater.dispatcher.add_handler(CommandHandler("whatsonFIP", get_live))
 updater.dispatcher.add_handler(CommandHandler("help", display_help))
 
 updater.start_polling()
